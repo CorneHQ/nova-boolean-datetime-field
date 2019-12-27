5940391dc05c0c4fcbadc6dc984518e2442e70db
Corné den Breejen <43491832+CorneHQ@users.noreply.github.com>
2019-12-27 13:52:28 +0100

    Update BooleanDatetime.php

--- src/BooleanDatetime.php
+++ src/BooleanDatetime.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PragmaRX\BooleanDatetime;
+namespace CorneHQ\BooleanDatetime;
 
 use Exception;
 use DateTimeInterface;
@@ -75,12 +75,6 @@ class BooleanDatetime extends Field
             $attribute,
             $resolveCallback ??
                 function ($value) {
-                    if (!$value instanceof DateTimeInterface) {
-                        throw new Exception(
-                            "DateTime field must cast to 'datetime' in Eloquent model."
-                        );
-                    }
-
                     return $value->format('Y-m-d H:i:s');
                 }
         );
